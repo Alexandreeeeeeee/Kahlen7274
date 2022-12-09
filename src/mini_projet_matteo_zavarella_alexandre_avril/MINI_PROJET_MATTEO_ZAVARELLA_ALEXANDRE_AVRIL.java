d82e86c6c4a9cd89f186b456920e8dad00bb485c
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-12-09 10:59:19 +0100

    TETS de

--- src/mini_projet_matteo_zavarella_alexandre_avril/MINI_PROJET_MATTEO_ZAVARELLA_ALEXANDRE_AVRIL.java
+++ src/mini_projet_matteo_zavarella_alexandre_avril/MINI_PROJET_MATTEO_ZAVARELLA_ALEXANDRE_AVRIL.java
@@ -1,24 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package mini_projet_matteo_zavarella_alexandre_avril;
 
-/**
- *
- * @author alexa
- */
+
 public class MINI_PROJET_MATTEO_ZAVARELLA_ALEXANDRE_AVRIL {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    int couille;
 
-    String pute;
-    int bb;
-        
+    }     
 }
